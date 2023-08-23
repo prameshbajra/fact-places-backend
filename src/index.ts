0b042763283631ce938ee261f4be8d7af7a141a4
prameshbajra <pe.messh@gmail.com>
2023-08-23 21:41:01 +0200

    Http post call in progress.

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,29 @@
-import {
-    onDocumentCreated,
-} from "firebase-functions/v2/firestore";
+import axios from "axios";
+import * as express from "express";
+import { Request, Response } from "express";
 import { setGlobalOptions } from "firebase-functions/v2";
+import { onRequest } from "firebase-functions/v2/https";
 
 setGlobalOptions({ maxInstances: 10 });
 
-exports.createItemOnPlaces = onDocumentCreated("places/{placeId}", (event) => {
-    const snapshot = event.data;
-    if (!snapshot) {
-        console.log("No data associated with the event");
+const app = express();
+app.use(express.json());
+
+app.post("/getPlacesData", async (req: Request, res: Response) => {
+    const { lat, lon } = req.body;
+
+    if (!lat || !lon) {
+        res.status(400).send({ error: "Lat and Lon are required!" });
         return;
     }
-    const data = snapshot.data();
-
-    console.log(data);
+    try {
+        const response = await axios.get(`https://nominatim.openstreetmap.org/reverse?lat=${lat}&lon=${lon}&format=json&accept-language=en`);
+        res.status(200).send(response.data);
+    } catch (error) {
+        res.status(500).send({
+            error: "Failed to fetch data from OpenStreetMap",
+        });
+    }
 });
+
+exports.app = onRequest(app);
