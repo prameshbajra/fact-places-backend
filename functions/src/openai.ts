047978847e9d8b79fff0b4c7f36567356b7db1e9
prsnnami <prasannamishra50@gmail.com>
2023-09-17 09:50:02 -0400

    change model to 16k and use custom format instructions to solve 3.5 model not understanding format instructions

--- functions/src/openai.ts
+++ functions/src/openai.ts
@@ -5,36 +5,51 @@ import { PromptTemplate } from "langchain/prompts";
 import { z } from "zod";
 import { TPlace } from "./model";
 
-
-const parser = StructuredOutputParser.fromZodSchema(z.array(
+const parser = StructuredOutputParser.fromZodSchema(
+  z.array(
     z.object({
-        place: z.string().describe("Name of the place"),
-        description: z.string().describe("A paragraph with at least 10 sentences that describes multiple fun facts about the place like the historical significance, who made it etc"),
+      place: z.string().describe("Name of the place"),
+      description: z
+        .string()
+        .describe(
+          "A paragraph with at least 10 sentences that describes multiple fun facts about the place like the historical significance, who made it etc"
+        ),
     })
-));
-const formatInstructions = parser.getFormatInstructions();
+  )
+);
+const formatInstructions = `
+Your output must follow the following example: \n
+[ 
+    { 
+        "place": place of interest,
+        "description": A paragraph with at least 10 sentences that describes multiple fun facts about the place like the historical significance, who made it etc
+    },
+    ...
+]
+`;
 
 const placesOfInterest = async (location: TPlace) => {
-    try {
-        const displayName = location.display_name;
-        const prompt = new PromptTemplate({
-            template:
-                `Answer the users question as best as possible. \n{format_instructions}
+  try {
+    const displayName = location.display_name;
+    const prompt = new PromptTemplate({
+      template: `Answer the users question as best as possible. \n
+                {format_instructions}
                 \n I am in at this place right now. {location}. I want you to give me the at least 5 specific places around this area which has very high historical significance and has a rich history behind it.`,
-            inputVariables: ["location"],
-            partialVariables: { format_instructions: formatInstructions },
-        });
-        const model = new OpenAI({ temperature: 0 });
-        const chain = new LLMChain({ llm: model, prompt: prompt });
-        const response = await chain.run(displayName);
-        console.log("Response: ", response);
-        // const parsedResponse = await parser.parse(response);
-        // return parsedResponse;
-        return response;
-    } catch (error) {
-        console.error("Error", error);
-        return [];
-    }
+      inputVariables: ["location"],
+      partialVariables: { format_instructions: formatInstructions },
+    });
+    const model = new OpenAI({
+      temperature: 0,
+      modelName: "gpt-3.5-turbo-16k",
+    });
+    const chain = new LLMChain({ llm: model, prompt: prompt });
+    const response = await chain.run(displayName);
+    const parsedResponse = await parser.parse(response);
+    return parsedResponse;
+  } catch (error) {
+    console.error("Error", error);
+    return [];
+  }
 };
 
 export { placesOfInterest };
